49b9c9bb34a1fabc50360edfaf8dfbb72f0b0dc4
Sydney Whilden <sydney.whilden25@uga.edu>
2021-11-30 21:43:59 -0500

    old edits

--- pyttitude/core.py
+++ pyttitude/core.py
@@ -1,1 +1,13 @@
 # don't know what goes here tbh
+
+# Imports
+import simpy as sp
+import ephem as ep
+import pygame as pg
+
+# Functions
+
+
+# Main program
+if __name__ == "__main__":
+    print("I work successfully thus far.")
